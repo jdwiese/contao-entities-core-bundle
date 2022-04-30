de0a334d2cda287d5f7a41def6e5008f72a51c36
Jan-Dirk Wiese <jan-dirk@awg88.de>
2022-04-30 10:55:20 +0200

    add dca helper class

---
+++ src/EventListener/DataContainer/HelperListener.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jdwiese\ContaoEntitiesCoreBundle\EventListener\DataContainer;
+
+use Contao\DataContainer;
+use DateTime;
+
+class HelperListener
+{
+    public function saveEmptyNull($varValue, DataContainer $dataContainer): ?string
+    {
+        return ('' === $varValue) ? null : $varValue;
+    }
+
+    public function saveBoolean($varValue, DataContainer $dataContainer): int
+    {
+        return $varValue ? 1 : 0;
+    }
+
+    public function saveDate($varValue, object $dataContainer): ?string
+    {
+        if ($varValue !== '') {
+            return date('Y-m-d', $varValue);
+        }
+        return null;
+    }
+
+    public function loadDate($varValue, DataContainer $dataContainer): ?int
+    {
+        if ($varValue) {
+            return DateTime::createFromFormat('Y-m-d', $varValue)->getTimestamp();
+        }
+        return null;
+    }
+
+    public function saveTime($varValue, object $dataContainer): ?string
+    {
+        if ((string)$varValue !== '') {
+            return date('H:i:s', $varValue);
+        }
+        return null;
+    }
+
+    public function loadTime($varValue, DataContainer $dataContainer): ?int
+    {
+        if ($varValue) {
+            return DateTime::createFromFormat('H:i:s', $varValue)->getTimestamp();
+        }
+        return null;
+    }
+}
