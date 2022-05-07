aa3c2b42c7bae120fa84abfd0224f87affa96fbc
Jan-Dirk Wiese <jan-dirk@awg88.de>
2022-05-07 13:07:50 +0200

    add mapped superclass for calendar events

--- src/Entity/TlCalendarEvents.php
+++ src/Entity/TlCalendarEvents.php
@@ -6,12 +6,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * TlCalendarEvents
- *
- * @ORM\Table(name="tl_calendar_events", indexes={
- *     @ORM\Index(name="alias", columns={"alias"}),
- *     @ORM\Index(name="pid_published_featured_start_stop", columns={"pid", "published", "featured", "start", "stop"})
- * })
- * @ORM\Entity(repositoryClass="Jdwiese\ContaoEntitiesCoreBundle\Repository\TlCalendarEventsRepository")
  */
 class TlCalendarEvents
 {
